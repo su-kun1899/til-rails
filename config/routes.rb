3cb4120fb75c66b255489aa115d82d6a7a40c816
su-kun1899 <higedrum.coz@gmail.com>
2016-12-31 00:55:24 +0900

    Redirect sample

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'helo/index'
   get 'helo', to: 'helo#index'
+  get 'helo/other'
 end
