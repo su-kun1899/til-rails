dd23db62c474e39a5447ee527290c89517fbaeee
su-kun1899 <higedrum.coz@gmail.com>
2016-12-31 18:31:07 +0900

    Use layout

--- app/controllers/helo_controller.rb
+++ app/controllers/helo_controller.rb
@@ -1,21 +1,11 @@
 class HeloController < ApplicationController
+  layout 'helo'
 
   def index
-    if request.post?
-      @title = 'Result'
-      # @msg = 'you typed: ' + params['input1'] +'.'
-      # @value = params['input1']
-      if params['check1']
-        @msg = 'you checked!'
-      else
-        @msg = 'not checked...'
-      end
-    else
-      @title = 'Index'
-      # @msg = 'type text...'
-      # @value = ''
-      @msg = 'check it...'
-    end
+    @header = 'layout sample'
+    @footer = 'copyright SYODA-Tuyano 2016.'
+    @title = 'New Layout'
+    @msg = 'this is sample page!'
   end
 
 end
