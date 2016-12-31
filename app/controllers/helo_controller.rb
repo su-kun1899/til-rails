4534e33f0521e6096d65f04e19dfe1c0da5e23e5
su-kun1899 <higedrum.coz@gmail.com>
2016-12-31 18:06:02 +0900

    Add check box

--- app/controllers/helo_controller.rb
+++ app/controllers/helo_controller.rb
@@ -3,12 +3,18 @@ class HeloController < ApplicationController
   def index
     if request.post?
       @title = 'Result'
-      @msg = 'you typed: ' + params['input1'] +'.'
-      @value = params['input1']
+      # @msg = 'you typed: ' + params['input1'] +'.'
+      # @value = params['input1']
+      if params['check1']
+        @msg = 'you checked!'
+      else
+        @msg = 'not checked...'
+      end
     else
       @title = 'Index'
-      @msg = 'type text...'
-      @value = ''
+      # @msg = 'type text...'
+      # @value = ''
+      @msg = 'check it...'
     end
   end
 
