ff0892bddfa139f5e223749a94646787b274d037
su-kun1899 <higedrum.coz@gmail.com>
2017-01-03 18:06:48 +0900

    Add find by id

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -3,4 +3,9 @@ class PeopleController < ApplicationController
     @msg = 'Person data.'
     @people = Person.all
   end
+
+  def show
+    @msg = 'Index data.'
+    @person = Person.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'people/index'
   get 'people', to: 'people#index'
+  get 'people/:id', to: 'people#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'helo/index'
